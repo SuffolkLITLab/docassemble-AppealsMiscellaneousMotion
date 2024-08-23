384a82d2b878e348cd72da1b2914fdddea4bbb79
Anonymous User <mark.marotta@su.suffolk.edu>
2024-08-23 11:43:43 -0400

    Meta data added with new fields

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ from distutils.util import convert_path
 
 standard_exclude = ('*.pyc', '*~', '.*', '*.bak', '*.swp*')
 standard_exclude_directories = ('.*', 'CVS', '_darcs', './build', './dist', 'EGG-INFO', '*.egg-info')
+
 def find_package_data(where='.', package='', exclude=standard_exclude, exclude_directories=standard_exclude_directories):
     out = {}
     stack = [(convert_path(where), '', package)]
@@ -45,7 +46,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
 setup(name='docassemble.AppealsMiscellaneousMotion',
       version='0.1.1',
       description=('Massachusetts Appeals Court miscellaneous motion'),
-      long_description='A Docassemble interview for a Massachusetts Appeals Court miscellaneous motion.',
+      long_description='# Massachusetts Appeals Court miscellaneous motion.\r\n\r\nThis is a Docassemble interview to complete a Massachusetts Appeals Court miscellaneous motion online. The live interview can be found at [https://www.courtformsonline.org/appeals/#miscmo](https://www.courtformsonline.org/appeals/#miscmo).',
       long_description_content_type='text/markdown',
       author='Mia Bonardi',
       author_email='mbonardi@su.suffolk.edu',
@@ -53,7 +54,7 @@ setup(name='docassemble.AppealsMiscellaneousMotion',
       url='https://docassemble.org',
       packages=find_packages(),
       namespace_packages=['docassemble'],
-      install_requires=['docassemble.AssemblyLine>=2.23.0'],
+      install_requires=['docassemble.AssemblyLine>=3.0.1'],
       zip_safe=False,
       package_data=find_package_data(where='docassemble/AppealsMiscellaneousMotion/', package='docassemble.AppealsMiscellaneousMotion'),
      )
